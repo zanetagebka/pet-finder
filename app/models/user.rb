20977cc5a7d7a2f29404b8c7f2ee0481248100a3
Žaneta Gebka <zaneta.gebka@easysoftware.com>
2021-01-14 22:51:51 +0100

    make changes in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:facebook, :google]
   rolify
 
+  has_one_attached :avatar
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
